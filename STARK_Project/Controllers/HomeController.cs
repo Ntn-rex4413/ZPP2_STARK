a4028aab00017eadb20843f6d9e63f7d91d56cb3
Michał Goc <mg054406@student.ath.edu.pl>
2021-03-28 19:25:44 +0200

    About Us update 2

--- STARK_Project/Controllers/HomeController.cs
+++ STARK_Project/Controllers/HomeController.cs
@@ -27,6 +27,10 @@ namespace STARK_Project.Controllers
         {
             return View();
         }
+        public IActionResult Aboutus()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
