ec13ec59948fd6be9f2d1ed6018eb5b9427ad08f
quezmes <quezmes@gmail.com>
2021-03-29 16:05:38 +0200

    Create crypto service interface

---
+++ STARK_Project/CryptoAPIService/ICryptoService.cs
@@ -0,0 +1,16 @@
+﻿using STARK_Project.CryptoApiModel;
+using STARK_Project.CryptoApiModel.CurrencyEnums;
+using STARK_Project.CryptoApiModel.CurrencySymbolsEnums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace STARK_Project.CryptoAPIService
+{
+    interface ICryptoService
+    {
+       CryptoInfo GetCryptocurrencyInfo(CryptocurrencySymbols cryptoSymbol, CurrencySymbols currencySymbol);
+        CryptoModel GetCryptocurrenciesInfo();
+    }
+}
