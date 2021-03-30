896f1de53dc8017192cacf4549689566bc97b9ba
JanB558 <JanB558@users.noreply.github.com>
2021-03-30 14:46:33 +0200

    added service to details

--- STARK_Project/Controllers/DetailsController.cs
+++ STARK_Project/Controllers/DetailsController.cs
@@ -3,11 +3,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using STARK_Project.CryptoAPIService;
 
 namespace STARK_Project.Controllers
 {
     public class DetailsController : Controller
     {
+        private readonly ICryptoService _service;
+        public DetailsController(ICryptoService service)
+        {
+            _service = service;
+        }
         public IActionResult Index()
         {
             return View();
