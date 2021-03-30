fee76df361ff4249b95b70c9dc7c6ac6e8cd9574
JanB558 <JanB558@users.noreply.github.com>
2021-03-30 14:41:47 +0200

    added service to summery controller

--- STARK_Project/Controllers/SummaryController.cs
+++ STARK_Project/Controllers/SummaryController.cs
@@ -3,13 +3,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using STARK_Project.CryptoApiModel;
+using STARK_Project.CryptoApiModel.CurrencyEnums;
+using STARK_Project.CryptoApiModel.CurrencySymbolsEnums;
+using STARK_Project.CryptoAPIService;
 
 namespace STARK_Project.Controllers
 {
     public class SummaryController : Controller
     {
+        private ICryptoService _service;
+        public SummaryController(ICryptoService service)
+        {
+            _service = service;
+        }
         public IActionResult Index()
         {
+
             return View();
         }
     }
