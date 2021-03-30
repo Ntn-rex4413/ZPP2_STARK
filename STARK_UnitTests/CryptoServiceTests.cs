dafa04e84c171a5b61c1ebdbb6bda17a6513dca6
JanB558 <JanB558@users.noreply.github.com>
2021-03-30 19:47:57 +0200

    CryptocurrencyDetails_CorrectData_IsTrue

--- STARK_UnitTests/CryptoServiceTests.cs
+++ STARK_UnitTests/CryptoServiceTests.cs
@@ -1,6 +1,7 @@
 using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using STARK_Project.CryptoApiModel.CurrencyEnums;
+using STARK_Project.CryptoApiModel.CurrencySymbolsEnums;
 using STARK_Project.CryptoAPIService;
 
 namespace STARK_UnitTests
@@ -8,6 +9,9 @@ namespace STARK_UnitTests
     [TestClass]
     public class CryptoServiceTests
     {
+        /// <summary>
+        /// gets all cryptocurrencies
+        /// </summary>
         [TestMethod]
         public void CryptocurrencySummary_Returns200_IsTrue()
         {
@@ -16,6 +20,18 @@ namespace STARK_UnitTests
             var data = service.GetCryptocurrenciesInfoAsync(CurrencySymbols.PLN).Result;
 
             Assert.Equals(data, 200);
+        }
+        /// <summary>
+        /// gets one cryptocurrency
+        /// </summary>
+        [TestMethod]
+        public void CryptocurrencyDetails_CorrectData_IsTrue()
+        {
+            var service = new CryptoService();
+
+            var data = service.GetCryptocurrencyInfoAsync(CryptocurrencySymbols.AAVE, CurrencySymbols.PLN).Result;
+
+            Assert.Equals(data.Price, 234697.7557);
         }
     }
 }
