3fe021fc168ca4ea19deb7d2c9b4ea0b69405a23
JanB558 <JanB558@users.noreply.github.com>
2021-03-31 13:57:43 +0200

    added website settings

---
+++ STARK_Project/Settings/WebsiteSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace STARK_Project.Settings
+{
+    public static class WebsiteSettings
+    {
+        public static string DefaultCryptocurrency { get; set; } = "BTC";
+        public static string DefaultCurrency { get; set; } = "PLN";
+    }
+}
