55b6403c6c235c7e5e98d815e96ed2f20f1233b1
Natan Jodkowski <nj054413@student.ath.edu.pl>
2021-04-03 23:27:02 +0200

    Added temporary code for Summary Index model

--- STARK_Project/Controllers/SummaryController.cs
+++ STARK_Project/Controllers/SummaryController.cs
@@ -10,7 +10,20 @@ namespace STARK_Project.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            // temp code - remove before merge
+            IEnumerable<CurrencyDummy> dummyCurrencies = new List<CurrencyDummy>
+            {
+                new CurrencyDummy{Name="Bitcoin"},
+                new CurrencyDummy{Name="Ethereum"}
+            };
+
+            return View(dummyCurrencies);
         }
+    }
+
+    // temp code - remove before merge
+    public class CurrencyDummy
+    {
+        public string Name { get; set; }
     }
 }
