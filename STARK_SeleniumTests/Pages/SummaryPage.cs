6e4077dbe7b32fc82bf3620e3b750650f8864ecb
JanB558 <JanB558@users.noreply.github.com>
2021-04-07 16:04:40 +0200

    folders and classes division (incomplete)

---
+++ STARK_SeleniumTests/Pages/SummaryPage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace STARK_SeleniumTests.Pages
+{
+    public class SummaryPage
+    {
+        private IWebDriver _driver { get; }
+        public SummaryPage(IWebDriver webDriver)
+        {
+            _driver = webDriver;
+        }
+
+        public IWebElement lnkDetails => _driver.FindElement(By.LinkText("Details"));
+        public IWebElement lblDetails => _driver.FindElement(By.LinkText("Zmiana w $"));
+
+        public void ClickDetails()
+        {
+            lnkDetails.Click();
+        }
+
+        public bool IsLabelVisible() => lblDetails.Displayed;
+    }
+}
