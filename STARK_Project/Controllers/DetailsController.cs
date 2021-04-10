bba8f2c4c709d8bcb9af78ccf050a086b0e21bf1
Michał Goc <mg054406@student.ath.edu.pl>
2021-04-10 14:00:54 +0200

    UnixTimeStampToDateTime

--- STARK_Project/Controllers/DetailsController.cs
+++ STARK_Project/Controllers/DetailsController.cs
@@ -20,5 +20,11 @@ namespace STARK_Project.Controllers
                 Enum.Parse<CurrencySymbols>(currency)).Result;
             return View(data);
         }
+        public static DateTime UnixTimeStampToDateTime(double unixTimeStamp)
+        {
+            System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            dtDateTime = dtDateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+            return dtDateTime;
+        }
     }
 }
