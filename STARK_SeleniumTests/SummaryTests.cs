61b1008d2cbab7447f3f35d27418483084a92d77
JanB558 <JanB558@users.noreply.github.com>
2021-04-13 14:33:42 +0200

    DetailsButton_RedirectsCorrectly_IsTrue()

--- STARK_SeleniumTests/SummaryTests.cs
+++ STARK_SeleniumTests/SummaryTests.cs
@@ -13,7 +13,7 @@ namespace STARK_SeleniumTests
         }
 
         [Test]
-        public void DetailsButtonRedirectsCorrectlyIsTrue()
+        public void DetailsButton_RedirectsCorrectly_IsTrue()
         {
             //open browser
             IWebDriver webDriver = new FirefoxDriver();
@@ -21,12 +21,13 @@ namespace STARK_SeleniumTests
             webDriver.Navigate().GoToUrl("https://localhost:44311/");
 
             //identify details button
-            IWebElement detailsLink = webDriver.FindElement(By.XPath("//*[contains(., 'Details')]"));
+            IWebElement detailsLink = webDriver.FindElement(By.CssSelector("[href*='Details?cryptocurrency=BTC']"));
 
             //operation
             detailsLink.Click();
 
-            var detailsLabel = webDriver.FindElement(By.XPath("//div[text() = 'Zmiana w $'"));
+            //var detailsLabel = webDriver.FindElement(By.XPath("//div[contains(text(),'Change % Daily')]"));
+            var detailsLabel = webDriver.FindElement(By.XPath("/html/body/div[1]/div/div/div/div/div[1]/h1"));
 
             //assertion
             Assert.That(detailsLabel.Displayed, Is.True);
