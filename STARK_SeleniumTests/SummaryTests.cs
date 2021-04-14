e17f502146c9045bff1319c97628b6a93c1294a8
JanB558 <JanB558@users.noreply.github.com>
2021-04-14 15:05:35 +0200

    SummaryTests refactoring, removal of obsolete tests

--- STARK_SeleniumTests/SummaryTests.cs
+++ STARK_SeleniumTests/SummaryTests.cs
@@ -6,59 +6,34 @@ namespace STARK_SeleniumTests
 {
     public class SummaryTests
     {
+        private IWebDriver webDriver;
+
         [SetUp]
         public void Setup()
         {
-
+            webDriver = new FirefoxDriver();
+            webDriver.Navigate().GoToUrl("https://localhost:44311/");
         }
 
         [Test]
-        public void CryptocurrencyName_CorrectlyDisplayed_IsTrue()
+        public void CryptocurrencyName_Displayed_IsTrue()
         {
-
-            IWebDriver webDriver = new FirefoxDriver();
-
-            webDriver.Navigate().GoToUrl("https://localhost:44311/");
-
-            IWebElement aurLabel = webDriver.FindElement(By.XPath("/html/body/div[1]/div/div/div/div/div[11]/div/div/div[1]/h6"));
+            IWebElement aurLabel = webDriver.FindElement(By.XPath("/html/body/div[1]/div/div/div/div/div[2]/div/div/div[1]/h6"));
 
             Assert.That(aurLabel.Displayed, Is.True);
         }
 
         [Test]
-        public void UpdateButton_CorrectlyDisplayed_IsTrue()
+        public void SubscribeButton_Displayed_IsTrue()
         {
-
-            IWebDriver webDriver = new FirefoxDriver();
-
-            webDriver.Navigate().GoToUrl("https://localhost:44311/");
-
-            IWebElement aurLabel = webDriver.FindElement(By.XPath("/html/body/div[1]/div/div/div/div/div[3]/div/div/div[2]/div/a[4]"));
-
-            Assert.That(aurLabel.Displayed, Is.True);
-        }
-
-        [Test]
-        public void SubscribeButton_CorrectlyDisplayed_IsTrue()
-        {
-
-            IWebDriver webDriver = new FirefoxDriver();
-
-            webDriver.Navigate().GoToUrl("https://localhost:44311/");
-
             IWebElement aurLabel = webDriver.FindElement(By.XPath("/html/body/div[1]/div/div/div/div/div[2]/div/div/div[2]/div/a[3]"));
 
             Assert.That(aurLabel.Displayed, Is.True);
         }
 
         [Test]
-        public void BuySellButton_CorrectlyDisplayed_IsTrue()
+        public void BuySellButton_Displayed_IsTrue()
         {
-
-            IWebDriver webDriver = new FirefoxDriver();
-
-            webDriver.Navigate().GoToUrl("https://localhost:44311/");
-
             IWebElement aurLabel = webDriver.FindElement(By.XPath("/html/body/div[1]/div/div/div/div/div[2]/div/div/div[2]/div/a[2]"));
 
             Assert.That(aurLabel.Displayed, Is.True);
@@ -66,13 +41,8 @@ namespace STARK_SeleniumTests
 
 
         [Test]
-        public void DetailsButton_CorrectlyDisplayed_IsTrue()
+        public void DetailsButton_Displayed_IsTrue()
         {
-
-            IWebDriver webDriver = new FirefoxDriver();
-
-            webDriver.Navigate().GoToUrl("https://localhost:44311/");
-
             IWebElement aurLabel = webDriver.FindElement(By.XPath("/html/body/div[1]/div/div/div/div/div[2]/div/div/div[2]/div/a[1]"));
 
             Assert.That(aurLabel.Displayed, Is.True);
