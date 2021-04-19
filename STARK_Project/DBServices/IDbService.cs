d68625cf89153c8d3a6b182ea37cc24f33b943b6
quezmes <quezmes@gmail.com>
2021-04-20 00:27:22 +0200

    Update IDbInterface about add to watchlist method

--- STARK_Project/DBServices/IDbService.cs
+++ STARK_Project/DBServices/IDbService.cs
@@ -9,5 +9,6 @@ namespace STARK_Project.DBServices
     interface IDbService
     {
         Task<bool> AddCryptocurrencyToDatabaseAsync(Cryptocurreny cryptocurreny);
+        Task<bool> AddToWatchListAsync(User user, Cryptocurreny cryptocurreny);
     }
 }
