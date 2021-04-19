a8e7c63647bcff5c4df09964f59f8782bc88a959
quezmes <quezmes@gmail.com>
2021-04-20 00:02:56 +0200

    Create Model info about Crytpocoin

---
+++ STARK_Project/CryptoApiModel/CryptoCoinProperty.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace STARK_Project.CryptoApiModel
+{
+    public class CryptoCoinList
+    {
+        public string Symbol { get; set; }
+        public string CoinName { get; set; }
+    }
+}
