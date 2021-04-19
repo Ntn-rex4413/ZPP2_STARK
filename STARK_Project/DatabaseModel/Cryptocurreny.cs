2dfb405633512b0a606d80e0f1949c26295b86a2
quezmes <quezmes@gmail.com>
2021-04-19 15:40:25 +0200

    Add Cryptocurrency Class to database Model

--- STARK_Project/DatabaseModel/Cryptocurreny.cs
+++ STARK_Project/DatabaseModel/Cryptocurreny.cs
@@ -1,13 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace STARK_Project.DatabaseModel
 {
-    public class User : IdentityUser
+    public class Cryptocurreny
     {
-
     }
 }
