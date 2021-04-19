f3e763bf43271a16684f643ba177e6d561be5f0e
quezmes <quezmes@gmail.com>
2021-04-19 15:39:07 +0200

    Extended User Class  by IdentityUser Class

--- STARK_Project/DatabaseModel/User.cs
+++ STARK_Project/DatabaseModel/User.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace STARK_Project.DatabaseModel
 {
-    public class User
+    public class User : IdentityUser
     {
     }
 }
