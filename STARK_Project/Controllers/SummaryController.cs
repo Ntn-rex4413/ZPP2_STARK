08e82ef64f3ee161f8b06c0407b23f26e44f1fdf
JanB558 <JanB558@users.noreply.github.com>
2021-04-20 09:30:24 +0200

    crypto to database no id

--- STARK_Project/Controllers/SummaryController.cs
+++ STARK_Project/Controllers/SummaryController.cs
@@ -34,157 +34,131 @@ namespace STARK_Project.Controllers
             //add new cryptocurrencies
             _dbService.AddCryptocurrencyToDatabaseAsync(new Cryptocurreny
             {
-                Id = 0,
                 Name = "Bitcoin",
                 Symbol = "BTC",
             });
             _dbService.AddCryptocurrencyToDatabaseAsync(new Cryptocurreny
             {
-                Id = 1,
                 Name = "Litecoin",
                 Symbol = "LTC",
             });
             _dbService.AddCryptocurrencyToDatabaseAsync(new Cryptocurreny
             {
-                Id = 2,
                 Name = "Namecoin",
                 Symbol = "NMC",
             });
             _dbService.AddCryptocurrencyToDatabaseAsync(new Cryptocurreny
             {
-                Id = 3,
                 Name = "Peercoin",
                 Symbol = "PPC",
             });
             _dbService.AddCryptocurrencyToDatabaseAsync(new Cryptocurreny
             {
-                Id = 4,
                 Name = "Dogecoin",
                 Symbol = "DOGE",
             });
             _dbService.AddCryptocurrencyToDatabaseAsync(new Cryptocurreny
             {
-                Id = 5,
                 Name = "Gridcoin",
                 Symbol = "GRC",
             });
             _dbService.AddCryptocurrencyToDatabaseAsync(new Cryptocurreny
             {
-                Id = 6,
                 Name = "Primecoin",
                 Symbol = "XPM",
             });
             _dbService.AddCryptocurrencyToDatabaseAsync(new Cryptocurreny
             {
-                Id = 7,
                 Name = "Ripple",
                 Symbol = "XRP",
             });
             _dbService.AddCryptocurrencyToDatabaseAsync(new Cryptocurreny
             {
-                Id = 8,
                 Name = "Nxt",
                 Symbol = "NXT",
             });
             _dbService.AddCryptocurrencyToDatabaseAsync(new Cryptocurreny
             {
-                Id = 9,
                 Name = "Auracoin",
                 Symbol = "AUR",
             });
             _dbService.AddCryptocurrencyToDatabaseAsync(new Cryptocurreny
             {
-                Id = 10,
                 Name = "Dash",
                 Symbol = "DASH",
             });
             _dbService.AddCryptocurrencyToDatabaseAsync(new Cryptocurreny
             {
-                Id = 11,
                 Name = "NEO",
                 Symbol = "NEO",
             });
             _dbService.AddCryptocurrencyToDatabaseAsync(new Cryptocurreny
             {
-                Id = 12,
                 Name = "MazaCoin",
                 Symbol = "MZC",
             });
             _dbService.AddCryptocurrencyToDatabaseAsync(new Cryptocurreny
             {
-                Id = 13,
                 Name = "Monero",
                 Symbol = "XMR",
             });
             _dbService.AddCryptocurrencyToDatabaseAsync(new Cryptocurreny
             {
-                Id = 14,
                 Name = "Titcoin",
                 Symbol = "TIT",
             });
             _dbService.AddCryptocurrencyToDatabaseAsync(new Cryptocurreny
             {
-                Id = 15,
                 Name = "Verge",
                 Symbol = "XVG",
             });
             _dbService.AddCryptocurrencyToDatabaseAsync(new Cryptocurreny
             {
-                Id = 16,
                 Name = "Stellar",
                 Symbol = "XLM",
             });
             _dbService.AddCryptocurrencyToDatabaseAsync(new Cryptocurreny
             {
-                Id = 17,
                 Name = "Vertcoin",
                 Symbol = "VTC",
             });
             _dbService.AddCryptocurrencyToDatabaseAsync(new Cryptocurreny
             {
-                Id = 18,
                 Name = "Ethereum",
                 Symbol = "ETC",
             });
             _dbService.AddCryptocurrencyToDatabaseAsync(new Cryptocurreny
             {
-                Id = 19,
                 Name = "Ethereum Classic",
                 Symbol = "ETC",
             });
             _dbService.AddCryptocurrencyToDatabaseAsync(new Cryptocurreny
             {
-                Id = 20,
                 Name = "Nano",
                 Symbol = "Nano",
             });
             _dbService.AddCryptocurrencyToDatabaseAsync(new Cryptocurreny
             {
-                Id = 21,
                 Name = "Tether",
                 Symbol = "USDT",
             });
             _dbService.AddCryptocurrencyToDatabaseAsync(new Cryptocurreny
             {
-                Id = 22,
                 Name = "Zcash",
                 Symbol = "ZEC",
             });
             _dbService.AddCryptocurrencyToDatabaseAsync(new Cryptocurreny
             {
-                Id = 23,
                 Name = "Bitcoin Cash",
                 Symbol = "BCH",
             });
             _dbService.AddCryptocurrencyToDatabaseAsync(new Cryptocurreny
             {
-                Id = 24,
                 Name = "EOS.IO",
                 Symbol = "EOS",
             });
             _dbService.AddCryptocurrencyToDatabaseAsync(new Cryptocurreny
             {
-                Id = 25,
                 Name = "Cardano",
                 Symbol = "ADA",
             });
