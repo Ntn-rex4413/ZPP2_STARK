04f7531b30e1d71c353980f6956a5b22d64524d4
Natan Jodkowski <nj054413@student.ath.edu.pl>
2021-04-23 18:09:36 +0200

    Cleanup of temp code before merge

--- STARK_Project/Controllers/SubscriptionsController.cs
+++ STARK_Project/Controllers/SubscriptionsController.cs
@@ -11,38 +11,8 @@ namespace STARK_Project.Controllers
     {
         public IActionResult Index()
         {
-            // Temporary code to test view before access to real data
-            var data = new DummySubscriptionsViewModel();
-
-            data.Cryptocurrencies = _temp_service.GetCryptocurrenciesInfoAsync().Result;
-            data.Currencies = _temp_service.GetCurrencies();
             return View();
         }
 
-        // Temporary constructor - remove before merge
-        public SubscriptionsController(ICryptoService service)
-        {
-            _temp_service = service;
-        }
-
-        // Temporary property - remove before merge
-        private readonly ICryptoService _temp_service;
-    }
-
-    // Dummy class - remove before merge
-    public class DummySubscriptionsViewModel
-    {
-        public List<Cryptocurreny> WatchedCryptocurrencies { get; set; }
-        public Dictionary<string, string> Cryptocurrencies { get; set; }
-        public Dictionary<string, string> Currencies { get; set; }
-    }
-
-    // Dummy class - remove before merge
-    public class Cryptocurreny
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Symbol { get; set; }
-        //public ICollection<User> Users { get; set; }
     }
 }
