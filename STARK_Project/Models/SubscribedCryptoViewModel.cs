fa11b7da0a80ceda16fadae446639fd62938cc26
Natan Jodkowski <nj054413@student.ath.edu.pl>
2021-04-23 21:51:29 +0200

    Set as inherited from Cryptocurreny, add reference

--- STARK_Project/Models/SubscribedCryptoViewModel.cs
+++ STARK_Project/Models/SubscribedCryptoViewModel.cs
@@ -1,11 +1,13 @@
-﻿using System;
+﻿using STARK_Project.DatabaseModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace STARK_Project.Models
 {
-    public class SubscribedCryptoViewModel
+    public class SubscribedCryptoViewModel : Cryptocurreny
     {
+
     }
 }
