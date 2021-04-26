a28a878d768380ccbb7d8365d3edd9ac349f28ae
JanB558 <JanB558@users.noreply.github.com>
2021-04-26 14:59:24 +0200

    brought back API call in SubscriptionsController

--- STARK_Project/Controllers/SubscriptionsController.cs
+++ STARK_Project/Controllers/SubscriptionsController.cs
@@ -26,6 +26,13 @@ namespace STARK_Project.Controllers
         }
         public async Task<IActionResult> Index(string currency = "USD")
         {
+            if (false)
+            {
+                //TODO
+                //remove at the very end
+                var coins = await _service.GetCryptocurrenciesAsync();
+                await _dbService.AddCryptocurrenciesToDatabaseAsync(coins.Select(x => new Cryptocurrency { Symbol = x.Key, Name = x.Value }).ToList());
+            }
             if (_userId == null)
             {
                 return RedirectToAction("Index", "Summary");
