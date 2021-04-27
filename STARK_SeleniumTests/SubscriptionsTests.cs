eb8f2ba0d47a1db5ca3bf1a85ff5176e49598e80
JanB558 <JanB558@users.noreply.github.com>
2021-04-27 10:44:31 +0200

    CryptocurrencyName_Displayed_IsTrue()

---
+++ STARK_SeleniumTests/SubscriptionsTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace STARK_SeleniumTests
+{
+    public class SubscriptionsTests
+    {
+        private IWebDriver webDriver;
+
+        [SetUp]
+        public void Setup()
+        {
+            webDriver = new FirefoxDriver();
+            webDriver.Navigate().GoToUrl("https://localhost:44311/Subscriptions");
+        }
+
+        [Test]
+        public void CryptocurrencyName_Displayed_IsTrue()
+        {
+            IWebElement element = webDriver.FindElement(By.XPath("/html/body/div[1]/div/div/div/div/div[3]/div/div/div[1]/h6[1]"));
+
+            Assert.That(element.Displayed, Is.True);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (webDriver != null)
+            {
+                webDriver.Dispose();
+            }
+        }
+    }
+}
