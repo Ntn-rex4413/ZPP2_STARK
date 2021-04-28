bed2a0f59a5a4cbf207a75a897bd64da5a821f2e
JanB558 <JanB558@users.noreply.github.com>
2021-04-28 18:48:29 +0200

    constructor

--- STARK_Project/Calculator/Calculator.cs
+++ STARK_Project/Calculator/Calculator.cs
@@ -2,13 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using STARK_Project.CryptoAPIService;
+using STARK_Project.DBServices;
 
 namespace STARK_Project.Calculator
 {
     public class Calculator
     {
+        private IDbService _dbService;
+        private ICryptoService _cryptoService;
+        public Calculator(IDbService dbService, ICryptoService cryptoService)
+        {
+            _dbService = dbService;
+            _cryptoService = cryptoService;
+        }
         public double Calculate(double valueLeft, string currencyLeft, string currencyRight)
         {
+            bool isLeftCurrencyCrypto;
+            bool isRightCurrencyCrypto;
+
             throw new NotImplementedException();
         }
     }
