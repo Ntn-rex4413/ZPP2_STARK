8dcd1dae17599d599774deed43e8c7b06f850e77
quezmes <quezmes@gmail.com>
2021-04-28 19:32:29 +0200

    Create Model structure for Historical data from API

---
+++ STARK_Project/CryptoApiModel/CryptoHistoricalData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace STARK_Project.CryptoApiModel
+{
+    public class CryptoHistoricalData
+    {
+        public CryptoHistoricalnfo[] Data { get; set; }
+    }
+}
---
+++ STARK_Project/CryptoApiModel/CryptoHistoricalnfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace STARK_Project.CryptoApiModel
+{
+    public class CryptoHistoricalnfo
+    {
+        public float Time { get; set; }
+        public float High { get; set; }
+    }
+}
