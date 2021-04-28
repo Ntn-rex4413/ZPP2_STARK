3370d242ba5227e79ca8f65ff0989780cfac0cf9
JanB558 <JanB558@users.noreply.github.com>
2021-04-28 18:45:49 +0200

    calculator class and interface

---
+++ STARK_Project/Calculator/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace STARK_Project.Calculator
+{
+    public class Calculator
+    {
+        public double Calculate(double valueLeft, string currencyLeft, string currencyRight)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface ICalculator
+    {
+        double Calculate(double valueLeft, string currencyLeft, string currencyRight);
+    }
+}
