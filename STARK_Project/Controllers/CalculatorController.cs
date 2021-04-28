0dc44570b020ae92b5258e22eb32d509c116dd6b
JanB558 <JanB558@users.noreply.github.com>
2021-04-28 19:13:09 +0200

    constructor

--- STARK_Project/Controllers/CalculatorController.cs
+++ STARK_Project/Controllers/CalculatorController.cs
@@ -2,14 +2,30 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using STARK_Project.CryptoAPIService;
+using STARK_Project.DBServices;
 
 namespace STARK_Project.Controllers
 {
     public class CalculatorController : Controller
     {
+        private readonly ICryptoService _service;
+        private readonly IDbService _dbService;
+
+        private readonly string _userId;
+
+        public CalculatorController(IHttpContextAccessor httpContextAccessor, ICryptoService service, IDbService dbService)
+        {
+            _service = service;
+            _dbService = dbService;
+            _userId = httpContextAccessor.HttpContext?.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        }
         public IActionResult Index(double valueLeft = 0, double valueRight = 0, string currencyLeft = "EUR", string currencyRight = "BTC")
         {
+
             return View();
         }
 
