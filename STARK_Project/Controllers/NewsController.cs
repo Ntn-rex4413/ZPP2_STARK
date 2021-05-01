0346e7e741b2f94cf4903548912e8c08d3549df9
JanB558 <JanB558@users.noreply.github.com>
2021-05-01 15:14:47 +0200

    rss controller and view

---
+++ STARK_Project/Controllers/NewsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace STARK_Project.Controllers
+{
+    public class NewsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
