7967543bef7368c00edbf5184679dc5efb718747
JanB558 <JanB558@users.noreply.github.com>
2021-05-05 14:26:59 +0200

    WebsiteCanLogInAndAccessSubscriptions

---
+++ STARK_SeleniumTests/KatalonTests/Website_CanLoginAndAccessSubscriptions_IsTrue.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace STARK_SeleniumTests.KatalonTests
+{
+    [TestFixture]
+    public class WebsiteCanLogInAndAccessSubscriptions
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new FirefoxDriver();
+            baseURL = "https://www.google.com/";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Test]
+        public void TheWebsiteCanLogInIsTrueTest()
+        {
+            driver.Navigate().GoToUrl("https://localhost:44311/Identity/Account/Login");
+            driver.FindElement(By.XPath("//div[@id='content']/div")).Click();
+            driver.FindElement(By.Id("Input_Email")).Click();
+            driver.FindElement(By.Id("Input_Email")).Clear();
+            driver.FindElement(By.Id("Input_Email")).SendKeys("lafel42099@iludir.com");
+            driver.FindElement(By.XPath("//div[@id='content']/div")).Click();
+            driver.FindElement(By.Id("Input_Password")).Click();
+            driver.FindElement(By.Id("Input_Password")).Clear();
+            driver.FindElement(By.Id("Input_Password")).SendKeys("Zpp2STARK!");
+            driver.FindElement(By.XPath("//button[@type='submit']")).Click();
+            driver.FindElement(By.XPath("//ul[@id='accordionSidebar']/li[3]/a/span")).Click();
+            driver.FindElement(By.LinkText("Kup / Sprzedaj")).Click();
+        }
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
