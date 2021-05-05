2a6c2801ddd157b37641c6d9a75c08cb5d927a6c
JanB558 <JanB558@users.noreply.github.com>
2021-05-05 14:39:49 +0200

    katalon test

---
+++ STARK_SeleniumTests/KatalonTests/CalculatorCanConvert.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace STARK_SeleniumTests.KatalonTests
+{
+    [TestFixture]
+    public class CalculatorCanConvert
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new FirefoxDriver();
+            baseURL = "https://www.google.com/";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Test]
+        public void TheCalculatorCanConvertTest()
+        {
+            driver.Navigate().GoToUrl("https://localhost:44311/Calculator");
+            driver.FindElement(By.Id("LeftValue")).Click();
+            driver.FindElement(By.Id("LeftValue")).Click();
+            driver.FindElement(By.Id("LeftValue")).Clear();
+            driver.FindElement(By.Id("LeftValue")).SendKeys("25");
+            driver.FindElement(By.XPath("(//button[@type='submit'])[2]")).Click();
+            driver.FindElement(By.XPath("//div[@id='content']/div/div/div/div/div/div/div/div/div/div[2]/div/div")).Click();
+        }
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
