2fd86986b8eebc245450257e11703a3a177a7f74
JanB558 <JanB558@users.noreply.github.com>
2021-05-05 14:18:06 +0200

    empty class for tests

---
+++ STARK_SeleniumTests/CryptoNewsTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace STARK_SeleniumTests
+{
+    public class CryptoNewsTests
+    {
+        private IWebDriver webDriver;
+
+        [SetUp]
+        public void Setup()
+        {
+            webDriver = new FirefoxDriver();
+            webDriver.Navigate().GoToUrl("https://localhost:44311/Calculator");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (webDriver != null)
+            {
+                webDriver.Dispose();
+            }
+        }
+    }
+}
