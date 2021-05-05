aebc88c7cb0d800aa4ab4778e0342b696ed43d77
quezmes <quezmes@gmail.com>
2021-05-05 16:35:26 +0200

    Add Condition database model

---
+++ STARK_Project/DatabaseModel/Condition.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace STARK_Project.DatabaseModel
+{
+    public class Condition
+    {
+        public int Id { get; set; }
+        public float TresholdMax { get; set; }
+        public float TresholdMin { get; set; }
+        public virtual Cryptocurrency Cryptocurrency { get; set; }
+        public virtual User User { get; set; }
+    }
+}
