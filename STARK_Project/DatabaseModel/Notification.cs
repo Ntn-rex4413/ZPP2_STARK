2d718e41f67b8812f2d8d888c9ad221261a01674
quezmes <quezmes@gmail.com>
2021-05-07 13:39:29 +0200

    Add notifiaction class to database model

---
+++ STARK_Project/DatabaseModel/Notification.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace STARK_Project.DatabaseModel
+{
+    public class Notification
+    {
+        public int Id { get; set; }
+        public string Message { get; set; }
+    }
+}
