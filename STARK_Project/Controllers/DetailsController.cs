35b7ba33085a5ef9d6a341765fd32d20a54fc34b
Natan Jodkowski <nj054413@student.ath.edu.pl>
2021-05-07 19:48:24 +0200

    Changed RedirectToAction to stay on currently viewed crypto and currency combination

--- STARK_Project/Controllers/DetailsController.cs
+++ STARK_Project/Controllers/DetailsController.cs
@@ -114,7 +114,7 @@ namespace STARK_Project.Controllers
         public async Task<IActionResult> RemoveNotification(int conditionId, string currentCrypto, string currentCurrency)
         {
             await _dbService.RemoveConditionAsync(_userId, conditionId);
-            return RedirectToAction("Index");
+            return RedirectToAction("Index", new { cryptocurrency = currentCrypto, currency = currentCurrency});
         }
 
         [HttpGet]
