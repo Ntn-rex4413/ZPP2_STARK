0a1c2e8cf12f031fe2303e446195126fb193636d
quezmes <quezmes@gmail.com>
2021-05-08 15:14:54 +0200

    Create Inftification service instance

---
+++ STARK_Project/NotificationServices/INotificationService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace STARK_Project.NotificationServices
+{
+    public interface INotificationService
+    {
+    }
+}
