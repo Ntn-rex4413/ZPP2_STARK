40f95df75f68a0341f82348d84439c4b62a50905
JanB558 <JanB558@users.noreply.github.com>
2021-05-08 15:01:45 +0200

    AlertDropDown_Displayed_IsTrue()

---
+++ STARK_SeleniumTests/GlobalLayout/LayoutTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace STARK_SeleniumTests.GlobalLayout
+{
+    public class LayoutTests
+    {
+        private IWebDriver webDriver;
+
+        [SetUp]
+        public void Setup()
+        {
+            webDriver = new FirefoxDriver();
+            webDriver.Navigate().GoToUrl("https://localhost:44311");
+        }
+
+        [Test]
+        public void AlertDropDown_Displayed_IsTrue()
+        {
+            IWebElement element = webDriver.FindElement(By.XPath("//*[@id=\"alertsDropdown\"]"));
+
+            Assert.That(element.Displayed, Is.True);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (webDriver != null)
+            {
+                webDriver.Dispose();
+            }
+        }
+    }
+}
