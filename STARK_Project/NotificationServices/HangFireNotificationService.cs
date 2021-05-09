83a6f3b868df25a34fb6eac33eaadd0a44b30745
quezmes <quezmes@gmail.com>
2021-05-09 10:41:26 +0200

    Add Create notification body method

--- STARK_Project/NotificationServices/HangFireNotificationService.cs
+++ STARK_Project/NotificationServices/HangFireNotificationService.cs
@@ -1,5 +1,8 @@
-﻿using STARK_Project.DatabaseModel;
+﻿using STARK_Project.CryptoAPIService;
+using STARK_Project.DatabaseModel;
+using STARK_Project.DBServices;
 using System;
+using Hangfire;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,9 +11,39 @@ namespace STARK_Project.NotificationServices
 {
     public class HangFireNotificationService : INotificationService
     {
+        private IDbService _dbService;
+        private ICryptoService _cyptoService;
+        private static readonly string _currency = "PLN";
+       
+        public HangFireNotificationService(IDbService dbService, ICryptoService cyptoService)
+        {
+            _dbService = dbService;
+            _cyptoService = cyptoService;
+        }
+
         public void CreateConditionNotify(string userId, Condition condition)
         {
-            throw new NotImplementedException();
+            var jobId = GetUniqueJobId(userId);
+            RecurringJob.AddOrUpdate(jobId, () => IsMaxTresholdExceeded(userId, condition, jobId), Cron.Hourly);
+        }
+
+        private async void IsMaxTresholdExceeded(string userId, Condition condition,string jobId)
+        {
+            
+            var coinInfo = await _cyptoService.GetCryptocurrencyInfoAsync(condition.Cryptocurrency.Symbol, _currency);
+            if(coinInfo.ChangeHour >= condition.TresholdMax)
+            {
+               await _dbService.AddNotification(userId, TresholdMaxMesssage(condition));
+                RecurringJob.RemoveIfExists(jobId);
+            }
+        }
+        private string GetUniqueJobId(string userId)
+        {
+            return userId + Guid.NewGuid();
+        }
+        private string TresholdMaxMesssage(Condition condition)
+        {
+            return $"Treshold max: {condition.TresholdMax} has benn exceeded in {condition.Cryptocurrency.Name} cryptocurrency!";
         }
     }
 }
