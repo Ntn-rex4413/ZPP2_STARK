2c5cf482403004cf1696f9780e00e4fe019d74d0
quezmes <quezmes@gmail.com>
2021-05-12 12:13:42 +0200

    Add Condition fuction for Trshold Min notification

--- STARK_Project/NotificationServices/HangFireNotificationService.cs
+++ STARK_Project/NotificationServices/HangFireNotificationService.cs
@@ -43,16 +43,34 @@ namespace STARK_Project.NotificationServices
             //RecurringJob.Trigger(jobId);
         }
 
-        public void TestMethod(int a, Condition c)
-        {
-            //var con = (Condition)c;
-            Console.WriteLine($"write line {a} {c.TresholdMax}");
-        }
         public async Task IsMaxTresholdExceeded(string userId, double value, string symbol, string jobId)
         {
             var coinInfo = await _cyptoService.GetCryptocurrencyInfoAsync(symbol, _currency);
             Console.WriteLine($"coin info {coinInfo}");
             if (coinInfo.Price >= value)
+            {
+                Console.WriteLine("Spelniono warunek!");
+                await _dbService.AddNotification(userId, TresholdMaxMesssage(value, symbol));
+                //RecurringJob.RemoveIfExists(jobId);
+            }
+        }
+
+        public void CreateConditionNotifyTresholdMin(string userId, Condition condition)
+        {
+            var jobId = GetUniqueJobId(userId);
+            _manager.AddOrUpdate(jobId, () => IsMinTresholdExceeded(userId,
+                condition.TresholdMax,
+                condition.Cryptocurrency.Symbol,
+                jobId),
+                "* * * * *");
+            //RecurringJob.Trigger(jobId);
+        }
+
+        public async Task IsMinTresholdExceeded(string userId, double value, string symbol, string jobId)
+        {
+            var coinInfo = await _cyptoService.GetCryptocurrencyInfoAsync(symbol, _currency);
+            Console.WriteLine($"coin info {coinInfo}");
+            if (coinInfo.Price <= value)
             {
                 Console.WriteLine("Spelniono warunek!");
                 await _dbService.AddNotification(userId, TresholdMaxMesssage(value, symbol));
