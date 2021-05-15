39afe4b6cffb7164c4f1a5e0414ab537e96bfdfd
JanB558 <JanB558@users.noreply.github.com>
2021-05-15 19:05:38 +0200

    controller + startup for calculator DI

--- STARK_Project/Startup.cs
+++ STARK_Project/Startup.cs
@@ -18,6 +18,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using STARK_Project.Calculator;
 
 namespace STARK_Project
 {
@@ -58,6 +59,9 @@ namespace STARK_Project
 
             services.AddControllersWithViews();
             services.AddRazorPages();
+
+            //calculator
+            services.AddScoped<ICalculator, CalculatorConverter>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
