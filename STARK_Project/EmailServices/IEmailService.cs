4ac38b0dabd3e505bfd2a638b4c6876bb1cb094d
quezmes <quezmes@gmail.com>
2021-05-17 18:28:45 +0200

    Add Email services

---
+++ STARK_Project/EmailServices/IEmailService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace STARK_Project.EmailServices
+{
+    interface IEmailService
+    {
+        Task SendEmail(string senderEmail, string recipientEmail,string title, string message);
+    }
+}
